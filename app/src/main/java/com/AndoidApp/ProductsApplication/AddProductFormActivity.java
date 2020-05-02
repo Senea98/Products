7e53810cf57e70583e75019d26ecbe3452d27d62
Senea98 <seneamarian998@gmail.com>
2020-05-02 20:03:31 +0300

    choose img, send data to Firebase

--- app/src/main/java/com/AndoidApp/ProductsApplication/AddProductFormActivity.java
+++ app/src/main/java/com/AndoidApp/ProductsApplication/AddProductFormActivity.java
@@ -1,18 +1,27 @@
 package com.AndoidApp.ProductsApplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class AddProductFormActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Uri imageUri;
+    private ImageView imageView;
 
     private Button addProduct, chooseImg;
     private EditText denumire, cantitate, pret, categorie;
@@ -26,6 +35,10 @@ public class AddProductFormActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_product_form);
 
         addProduct = findViewById(R.id.AddProduct);
+        chooseImg = findViewById(R.id.chooseImg);
+        imageView = findViewById(R.id.ImgView);
+
+
 
         addProduct.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +51,7 @@ public class AddProductFormActivity extends AppCompatActivity {
                 pret=findViewById(R.id.pret);
                 categorie=findViewById(R.id.categorie);
 
+
                 String dentxt = denumire.getText().toString();
                 String cantxt = cantitate.getText().toString();
                 String pretxt = pret.getText().toString();
@@ -48,10 +62,36 @@ public class AddProductFormActivity extends AppCompatActivity {
                 else if(pretxt.isEmpty())pret.setError("Camp obligatoriu");
                 else if(categtxt.isEmpty())categorie.setError("Camp obligatoriu");
                 else{
-
+                    Produs obj = new Produs(dentxt, cantxt, pretxt, categtxt);
+                    dbref= FirebaseDatabase.getInstance().getReference().child("Prods");
+                    dbref.push().setValue(obj);
                 }
+
             }
         });
 
+        chooseImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenFileChooser();
+            }
+        });
+    }
+    private void OpenFileChooser(){
+        Intent intent = new Intent();
+        intent.setType("image/");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imageUri = data.getData();
+
+            Picasso.with(this).load(imageUri).into(imageView);
+        }
     }
 }
---
+++ app/src/main/java/com/AndoidApp/ProductsApplication/Produs.java
@@ -0,0 +1,12 @@
+package com.AndoidApp.ProductsApplication;
+
+class Produs {
+    public String denumire, cantitate, pret, categorie;
+
+    public Produs(String denumire, String cantitate, String pret, String categorie) {
+        this.denumire = denumire;
+        this.cantitate = cantitate;
+        this.pret = pret;
+        this.categorie = categorie;
+    }
+}
